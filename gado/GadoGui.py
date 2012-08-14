d25a2aade0a1f279f4722ed255361df36d2cd8b9
Robert Douglas <robert@esdallc.com>
2012-08-14 17:53:03 -0400

    Moved around layout to accommodate the last scanned image and last webcam picture so the user can make sure the machine is working properly

--- gado/GadoGui.py
+++ gado/GadoGui.py
@@ -1,6 +1,7 @@
 import sys
 from Tkinter import *
 import tkMessageBox
+import Image, ImageTk
 import ttk
 #from gado.ProgressMeter import *
 import Pmw
@@ -15,6 +16,10 @@ class GadoGui(Frame):
     
     #Widgets
     global artifactSetDropDown
+    
+    #These have to be globals in order to avoid garbage collection bug in TkImage class
+    global frontImage
+    global backImage
     
     def _demo(meter, value):
         meter.set(value)
@@ -123,6 +128,9 @@ class GadoGui(Frame):
         #Create the status center
         self.createStatusWidgets()
         
+        #Create the image display widgets
+        self.createImageDisplayWidgets()
+        
     def createConnectionControlWidgets(self):
         #Create the label
         self.connectionLabel = Label(self)
@@ -159,11 +167,11 @@ class GadoGui(Frame):
         #Create label
         artifactSetLabel = Label(self)
         artifactSetLabel["text"] = "Artifact Set: "
-        artifactSetLabel.grid(row=3, column=0, sticky=W, padx=10, pady=5)
+        artifactSetLabel.grid(row=0, column=0, sticky=W, padx=10, pady=5, columnspan=2)
         
         #Create dropdown for selecting an artifact set
         set_dropdown = Pmw.ComboBox(self, selectioncommand=self.set_selected_set)
-        set_dropdown.grid(row=4, column=0, sticky=N+S+E+W, padx=10, pady=5, columnspan=2)
+        set_dropdown.grid(row=1, column=0, sticky=N+S+E+W, padx=10, pady=5, columnspan=1)
         self.set_dropdown = set_dropdown
         self._populate_set_dropdown()
         
@@ -171,7 +179,7 @@ class GadoGui(Frame):
         new_set_button = Button(self)
         new_set_button["text"] = "New Artifact Set"
         new_set_button["command"] = self.manage_sets.show
-        new_set_button.grid(row=4, column=2, sticky=N+S+E+W, padx=10, pady=5, columnspan=2)
+        new_set_button.grid(row=1, column=1, sticky=N+S+E+W, padx=10, pady=5, columnspan=1)
     
     def set_selected_set(self, a):
         idx = self.set_dropdown.curselection()[0]
@@ -183,39 +191,74 @@ class GadoGui(Frame):
         #Create label
         self.controlLabel = Label(self)
         self.controlLabel["text"] = "Control: "
-        self.controlLabel.grid(row=5, column=0, sticky=W, padx=10, pady=5)
+        self.controlLabel.grid(row=0, column=2, sticky=W, padx=10, pady=5)
         
         #Create start/stop/pause/restart buttons
         self.startButton = Button(self)
         self.startButton["text"] = "Start"
         self.startButton["command"] = self.startRobot
-        self.startButton.grid(row=6, column=0, sticky=N+S+E+W, padx=10, pady=5)
+        self.startButton.grid(row=1, column=2, sticky=N+S+E+W, padx=10, pady=5)
         
         self.pauseButton = Button(self)
         self.pauseButton["text"] = "Pause"
         self.pauseButton["command"] = self.pauseRobot
-        self.pauseButton.grid(row=6, column=1, sticky=N+S+E+W, padx=10, pady=5)
+        self.pauseButton.grid(row=1, column=3, sticky=N+S+E+W, padx=10, pady=5)
         
         self.stopButton = Button(self)
         self.stopButton["text"] = "Stop"
         self.stopButton["command"] = self.stopRobot
-        self.stopButton.grid(row=6, column=2, sticky=N+S+E+W, padx=10, pady=5)
+        self.stopButton.grid(row=1, column=4, sticky=N+S+E+W, padx=10, pady=5)
         
         self.resetButton = Button(self)
         self.resetButton["text"] = "Reset"
         self.resetButton["command"] = self.resetRobot
-        self.resetButton.grid(row=6, column=3, sticky=N+S+E+W, padx=10, pady=5)
+        self.resetButton.grid(row=1, column=5, sticky=N+S+E+W, padx=10, pady=5)
         
     def createStatusWidgets(self):
         #Create label
         self.statusLabel = Label(self)
         self.statusLabel["text"] = "Status: "
-        self.statusLabel.grid(row=8, column=0, sticky=W, padx=10, pady=5)
+        self.statusLabel.grid(row=4, column=0, sticky=W, padx=10, pady=5)
         
         #Actual message box
         self.messageEntry = Entry(self)
-        self.messageEntry.grid(row=9, column=0, columnspan=4, sticky=N+S+E+W, padx=10, pady=5)
-    
+        self.messageEntry.grid(row=5, column=0, columnspan=6, sticky=N+S+E+W, padx=10, pady=5)
+        
+    def createImageDisplayWidgets(self):
+        #Scanner label
+        self.scannerLabel = Label(self)
+        self.scannerLabel["text"] = "Scanner Image:"
+        self.scannerLabel.grid(row=2, column=0, sticky=W, padx=10, pady=5)
+        
+        image = Image.open("C:\\Users\\Robert\\test.png")
+        image.thumbnail((500, 500), Image.ANTIALIAS)
+        
+        image2 = Image.open("C:\\Users\\Robert\\test.bmp")
+        image2.thumbnail((500,500), Image.ANTIALIAS)
+        #image = image.resize((500, 500), Image.ANTIALIAS)
+        
+        self.frontImage = ImageTk.PhotoImage(image)
+        self.backImage = ImageTk.PhotoImage(image2)
+        
+        imgLabel = Label(self, image=self.frontImage)
+        imgLabel.photo = self.frontImage
+        imgLabel.grid(row=3, column=0, sticky=N+S+E+W, padx=10, pady=5, columnspan=2)
+        
+        imgLabel2 = Label(self, image=self.backImage)
+        imgLabel2.photo = self.backImage
+        imgLabel2.grid(row=3, column=2, sticky=N+S+E+W, padx=10, pady=5, columnspan=4)
+        
+        #canvas = Canvas(self, bg="black", width=self.frontImage.width(), height=self.frontImage.height())
+        #grid(row=10, column=0, sticky=N+S+E+W, padx=10, pady=5, columnspan=4)
+        #print "w: %s, h: %s" % (self.frontImage.width(), self.frontImage.height())
+        #canvas.place(x=0, y=0)
+        #canvas.pack()
+        #canvas.place(x=0, y=0, width=self.frontImage.width(), height=self.frontImage.height())
+        #canvas.grid(row=10, column=0, columnspan=1, sticky=N+S+E+W, padx=10, pady=5)
+        #canvas.create_image(0, 0, image=self.frontImage)
+        #labelImage = Label(self, image=tkImage)
+        #labelImage.place(x=0, y=0, width=image.size[0], height=image.size[1])
+        #labelImage.grid(row=10, column=0, sticky=N+S+E+W, padx=10, pady=5, columnspan=4)
         
     #################################################################################
     #####                           FUNCTION WRAPPERS                           #####
