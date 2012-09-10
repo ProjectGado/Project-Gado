956a2f2393ecaf0368f5684bdd346a6c22de9266
Robert Douglas <robert@esdallc.com>
2012-09-10 15:51:46 -0400

    Added ability to write out logs to a Log folder either in the cwd (Unix) or the AppData\Gado directory (Windows)

--- src/gado/Logger.py
+++ src/gado/Logger.py
@@ -1,15 +1,32 @@
-import logging
+import logging, os, sys
+from gado.functions import *
 
 #Defintions
-LOGGING_LEVEL = logging.DEBUG
+LOGGING_LEVEL = logging.ERROR
 
 class Logger:
     
     def __init__(self, moduleName):
         
+        #Make sure we have a logging directory we can write to
+        try:
+            path = os.mkdir(os.path.join(gadodir(), 'Logs'))
+        except:
+            if os.name == 'nt':
+                path = os.path.join(gadodir(), 'Logs')
+            else:
+                path = os.path.join('.', 'Logs')
+            pass
+            
+        #Get an instance of the logger with the passed in module name
         self.logger = logging.getLogger(moduleName)
         
+        #Set the currently selected log level
         self.logger.setLevel(LOGGING_LEVEL)
+        
+        #Create the file handler
+        fh = logging.FileHandler(os.path.join(path, 'gado.log'))
+        fh.setLevel(LOGGING_LEVEL)
         
         # create console handler and set level to debug
         ch = logging.StreamHandler()
@@ -20,10 +37,15 @@ class Logger:
         
         # add formatter to ch
         ch.setFormatter(formatter)
+        fh.setFormatter(formatter)
         
         # add ch to logger
         self.logger.addHandler(ch)
         
+        #Add the file handler to the logger
+        self.logger.addHandler(fh)
+        
     def getLoggerInstance(self):
         
+        #Return an instance of the configured logger object
         return self.logger
